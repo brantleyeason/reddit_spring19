4dde46efad7906ee6c6eac5b10510ac66ac878dd
Brantley Eason <brantleyeason@Brantleys-MacBook-Pro.local>
2019-02-27 19:07:04 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # controller, pound sign, action
+  root 'subs#index'
+  
+  # get is an http request
+  # get 'subs/index'
+
+  resources :subs do  
+    resources :topics
+  end
+
+  # sub_id 7
+  # topic_id 4
+
+  # /subs/7/topics/4
+
+
+
+
+# get '/subs', to: 'subs#index'
+# get '/subs/:id', to: 'subs#show'
+# get '/subs/:id', to: 'subs#edit'
+# put '/subs/:id', to: 'subs#update'
+# delete '/subs/:id', to: 'subs#destroy'
+
+
 end
